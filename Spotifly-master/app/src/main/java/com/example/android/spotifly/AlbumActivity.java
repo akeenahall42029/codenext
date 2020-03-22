633eb9d1ffe256697be55d87d9e0b21d26cc46a3
admin <admin@MacBook-Pro-91.local>
2020-03-22 13:26:02 -0400

    Functionality of my album section

--- Spotifly-master/app/src/main/java/com/example/android/spotifly/AlbumActivity.java
+++ Spotifly-master/app/src/main/java/com/example/android/spotifly/AlbumActivity.java
@@ -3,6 +3,7 @@ package com.example.android.spotifly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -15,6 +16,13 @@ public class AlbumActivity extends AppCompatActivity {
         setContentView(R.layout.activity_album);
         //album and artist have similar charicteristics; reusing template
         ArrayList<Album> albums = new ArrayList<>();
-        albums.add(new Album(R.drawable.ic_launcher_background, "Pop Smoke", "Meet the Woo 2"));
+        albums.add(new Album(R.drawable.popalbum, "Pop Smoke", "Meet the Woo 2"));
+        albums.add(new Album(R.drawable.ririalbum, "Rihanna", "ANTI"));
+        albums.add(new Album(R.drawable.beyoncealbum, "Beyonce", "Lemonade"));
+        //linking listview inside of album activty
+        ListView albumListView = findViewById(R.id.album_listview);
+        AlbumAdapter albumAdapter = new AlbumAdapter(this,albums);
+        albumListView.setAdapter(albumAdapter);
+
     }
 }
--- Spotifly-master/app/src/main/java/com/example/android/spotifly/AlbumAdapter.java
+++ Spotifly-master/app/src/main/java/com/example/android/spotifly/AlbumAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,6 +24,13 @@ public class AlbumAdapter extends ArrayAdapter<Album> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         convertView = LayoutInflater.from(getContext()).inflate(R.layout.category_item_album, parent, false);
         Album currentAlbum = getItem(position);
+        ImageView currentAlbumImageView = convertView.findViewById(R.id.albumCover);
+        TextView albumArtistName = convertView.findViewById(R.id.artistName);
+        TextView albumName = convertView.findViewById(R.id.albumName);
+        currentAlbumImageView.setImageResource(currentAlbum.getAlbumCover());
+        albumArtistName.setText(currentAlbum.getArtistName());
+        albumName.setText(currentAlbum.getAlbumName());
+
         return convertView;
 
 
